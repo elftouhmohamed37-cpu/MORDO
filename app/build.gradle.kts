aff47e582c680baac3645ef6fccceeec553824e0
elftouhmohamed37-cpu <elftouhmohamed37@gmail.com>
2026-09-26 00:53:01 +0100

    Add app/build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.mordo.app"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.mordo.app"
+        minSdk = 26
+        targetSdk = 36
+        versionCode = 2
+        versionName = "0.2.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions { jvmTarget = "17" }
+}
